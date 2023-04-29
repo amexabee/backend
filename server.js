f8c60ec689150b53e027452a89a091fdd91a8ef8
Amanuel Galema <amexabee@yahoo.com>
2023-04-29 22:04:10 +0300

    Add .env

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 
 // express app
 const app = express();
 
+// routes
 app.get('/', (req, res) => {
-  res.json({ mssg: 'hi' });
+  res.json({ mssg: 'Hi' });
 });
 
 // listen for requests
-app.listen(4000, () => {
-  console.log('listening on port 4000');
+app.listen(process.env.PORT, () => {
+  console.log('listening on port', process.env.PORT);
 });

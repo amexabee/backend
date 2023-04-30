d61e0ffe9fda40256b30da5221baf565d177bf1d
Amanuel Galema <amexabee@yahoo.com>
2023-04-30 10:05:27 +0300

    Install mongoose, connect to database

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 const cors = require('cors');
 
@@ -16,7 +17,13 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT);
-});
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db and listening on port', process.env.PORT);
+    });
+  })
+  .catch((err) => console.log(err));

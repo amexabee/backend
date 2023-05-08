673d3abb7c2c41246f4b81175ea2d2a61a959ee0
Amanuel Galema <amexabee@yahoo.com>
2023-05-08 17:38:29 +0300

    Update createWorkout function

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -28,7 +28,7 @@ const createWorkout = async (req, res) => {
     const workout = await Workout.create({ title, reps, load });
     res.status(200).json(workout);
   } catch (error) {
-    res.status(400).json(req.body);
+    res.status(400).json({ error: error.message });
   }
 };
 

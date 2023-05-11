ce33f800ccda065929e6be525eebf801ceaf61c7
Amanuel Galema <amexabee@yahoo.com>
2023-05-11 13:28:43 +0300

    Fix error handling logic

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -23,7 +23,19 @@ const getWorkout = async (req, res) => {
 
 // create new workout
 const createWorkout = async (req, res) => {
-  const { title, reps, load } = req.body;
+  const { title, load, reps } = req.body;
+
+  const emptyFields = [];
+
+  if (!title) emptyFields.push('title');
+  if (!load) emptyFields.push('load');
+  if (!reps) emptyFields.push('reps');
+
+  if (emptyFields.length > 0)
+    return res
+      .status(400)
+      .json({ error: 'Please fill all the fields', emptyFields });
+
   try {
     const workout = await Workout.create({ title, reps, load });
     res.status(200).json(workout);

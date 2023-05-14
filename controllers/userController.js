e3c8b26f87c57cebabd8d737d31889a6b340d85b
Amanuel Galema <amexabee@yahoo.com>
2023-05-14 22:40:20 +0300

    Add user routes, controller and model

---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+// login user
+const loginUser = async (req, res) => {
+  res.json({ mssg: 'login' });
+};
+
+// login user
+const signupUser = async (req, res) => {
+  res.json({ mssg: 'login' });
+};
+
+module.exports = { loginUser, signupUser };

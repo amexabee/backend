dd3418e1245eb953a3423abe872af68b3ffea9e6
Amanuel Galema <amexabee@yahoo.com>
2023-05-15 20:04:16 +0300

    Add middleware to protect unauthorized usage

--- routes/workouts.js
+++ routes/workouts.js
@@ -7,8 +7,11 @@ const {
   deleteWorkout,
   updateWorkout,
 } = require('../controllers/workoutController');
+const requireAuth = require('../middleware/requireAuth');
 
 const router = express.Router();
+
+router.use(requireAuth);
 
 // GET all workouts
 router.get('/', getWorkouts);

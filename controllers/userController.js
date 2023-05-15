7e6aaacfd74093b054e5376d3d16e99bd337ddf9
Amanuel Galema <amexabee@yahoo.com>
2023-05-15 03:18:28 +0300

    Complete logic to login a user

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,10 +7,18 @@ const createToken = (_id) => {
 
 // login user
 const loginUser = async (req, res) => {
-  res.json({ mssg: 'login' });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
-// login user
+// signup user
 const signupUser = async (req, res) => {
   const { email, password } = req.body;
 
